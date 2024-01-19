92a66535dad2e8105b787bebf17ac107cf6dad48
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-01-19 15:40:52 -0300

    FEAT login form

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './profile.components';
+import { LoginComponent } from './login.component';
 
 export const userRoutes: Routes = [
   { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
 ];
