d8e800b12cc199bccc5dfa8a767fdc74c64069af
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-01-22 14:36:34 -0300

    FEAT valid login

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,10 +4,20 @@ import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
   userName;
   password;
+  mouseoverLogin;
 
   constructor(private authService: AuthService, private router: Router) {}
 
