20f579a002b7a9ae2ed31c346fe8b5c80b4e1f19
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-02-21 14:40:29 -0300

    FEAT logout funcionality

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -68,4 +68,13 @@ export class AuthService {
       options
     );
   }
+
+  logout() {
+    this.currentUser = undefined;
+
+    let options = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+    return this.http.post('/api/logout', {}, options);
+  }
 }
