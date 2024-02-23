54f41f4e382f13070381389c96e533ea96ee7bcc
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-02-23 14:27:05 -0300

    FEAT test voter service add method

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -17,8 +17,23 @@ describe('VoterService', () => {
 
       voterService.deleteVoter(3, <ISession>session, 'joe');
 
-      expect(session.voters.length).toBe(1)
-      expect(session.voters[0]).toBe("john")
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe('john');
+    });
+  });
+
+  describe('addVoter', () => {
+    it('should call http.post with the right URL', () => {
+      var session = { id: 6, voters: ['joe', 'john'] };
+      mockHttp.post.and.returnValue(of(false));
+
+      voterService.addVoter(3, <ISession>session, 'joe');
+
+      expect(mockHttp.post).toHaveBeenCalledWith(
+        '/api/events/3/sessions/6/voters/joe',
+        {},
+        jasmine.any(Object)
+      );
     });
   });
 });
